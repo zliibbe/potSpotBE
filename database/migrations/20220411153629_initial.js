9af01ed6622b919af730e6c1148c683a9e4b98f1
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-04-11 09:48:43 -0600

    Create migration to create potholes and pictures tables

---
+++ database/migrations/20220411153629_initial.js
@@ -0,0 +1,34 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('potholes', function (table) {
+      table.increments('id').primary();
+      table.string('longitude');
+      table.string('latitude');
+      table.string('description');
+
+      table.timestamps(true, true);
+    })
+    .createTable('pictures', function (table) {
+      table.increments('id').primary();
+      table.string('url')
+      table.integer('pothole_id').unsigned()
+      table.foreign('pothole_id')
+        .references('potholes.id');
+
+      table.timestamps(true, true);
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema
+    .dropTable('pictures')
+    .dropTable('potholes')
+};
