6fac29e27875355d454bfa978e16d01d61eae200
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-04-12 09:22:05 -0600

    Refactor to return id and message upon posting a new pothole

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 
-app.set('port', process.env.PORT || 3002);
+app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Pot Spot';
 
 app.get('/', (request, response) => {
@@ -17,8 +17,10 @@ app.get('/api/v1/potholes', async (request, response) => {
   try {
     const potholes = await database('potholes').select();
     response.status(200).json(potholes);
-  } catch(error) {
-    response.status(500).json({ error });
+  } catch (error) {
+    response.status(500).json({
+      error
+    });
   }
 });
 
@@ -26,30 +28,33 @@ app.get('/api/v1/pictures', async (request, response) => {
   try {
     const pictures = await database('pictures').select('url');
     response.status(200).json(pictures);
-  } catch(error) {
-    response.status(500).json({ error });
+  } catch (error) {
+    response.status(500).json({
+      error
+    });
   }
 });
 
 app.post('/api/v1/potholes', async (request, response) => {
 
-  let id;
-
   const pothole = request.body;
 
-    if(!pothole) {
-        return response.status(400).send({ error: `Where's the body?`, request: `${request}`})
-    }
+  if (!pothole) {
+    return response.status(400).send({
+      error: `Where's the body?`,
+      request: `${request}`
+    })
+  }
 
   for (let requiredParameter of ['latitude', 'longitude', 'description', 'pictures']) {
     if (!pothole[requiredParameter]) {
       return response
         .status(422)
-        .send({ error: `Expected format: { latitude: <String>, longitude: <String>, description: <String>, pictures: <Array> }. You're missing a "${requiredParameter}" property.` });
+        .send({
+          error: `Expected format: { latitude: <String>, longitude: <String>, description: <String>, pictures: <Array> }. You're missing a "${requiredParameter}" property.`
+        });
     }
   }
-
-  const {latitude, longitude, description} = pothole;
 
   const createPothole = async (ph) => {
 
@@ -60,21 +65,17 @@ app.post('/api/v1/potholes', async (request, response) => {
     }, 'id');
 
     const pics = Object.values(ph.pictures)
-    id = potholeId;
-    console.log(typeof pics);
-    // const pics = JSON.parse(`${ph.pictures}`)
-    // console.log(pics)
-
-
+    let id = potholeId;
     let picturePromises = pics.map(picture => {
-      console.log(picture);
       return createPicture({
         url: picture,
         pothole_id: potholeId[0].id
       })
     });
 
-    return Promise.all(picturePromises);
+    return Promise.all(picturePromises).then(() => {
+      return id
+    });
   };
 
   const createPicture = (picture) => {
@@ -82,15 +83,13 @@ app.post('/api/v1/potholes', async (request, response) => {
   };
 
   try {
-    // const id = await database('potholes').insert({latitude, longitude, description}, 'id');
-    // const picturePromises = pothole.pictures.map(picture => {
-    //   knex('pictures').insert({url: picture, pothole_id: id})
-    // });
-    // Promise.all(picturePromises)
-    createPothole(pothole)
-    return response.status(201).json({ id })
+    let idPromise = await createPothole(pothole)
+    console.log(idPromise[0].id)
+    return response.status(201).json({id: idPromise[0].id, message:`Your pothole has been added.`})
   } catch (error) {
-    response.status(500).json({error: `Ya done goofed ${error}`});
+    response.status(500).json({
+      error: `Your pothole was not added see Error: ${error}`
+    });
   }
 });
 
