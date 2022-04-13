f5651a74e0393b0569640fd6b956118642ccb009
Jake Dunafon <jlad795@gmail.com>
2022-04-13 15:41:00 -0700

    Add seeds path into production

--- knexfile.js
+++ knexfile.js
@@ -27,6 +27,9 @@ module.exports = {
     migrations: {
       directory: './database/migrations'
     },
+    seeds: {
+      directory: './database/seeds/dev'
+    },
     useNullAsDefault: true
   }
 
