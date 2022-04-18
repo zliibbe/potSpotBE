f231fef348d22c00c19ad6fe99d55e44c1eb3402
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-04-18 13:57:06 -0600

    Create usit test for api/v1/pictures endpoint

--- test/server_spec.js
+++ test/server_spec.js
@@ -52,4 +52,34 @@ describe('Pot Spot API routes!', () => {
         expect(res.body[0]).to.have.property('updated_at');
       })
   })
+
+
+
+  it('should return all the photos @ /api/v1/pictures', () => {
+    const testPic1 = {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Asphalt_deterioration.jpg/1024px-Asphalt_deterioration.jpg',
+      pothole_id: 2
+    }
+
+    const testPic2 = {
+      url: 'https://www.thebalance.com/thmb/VlnrT3pRKvtegoumE0fXWmA4pWI=/2121x1193/smart/filters:no_upscale()/pothole-174662203-5a7dc84aae9ab80036c6ad36.jpg',
+      pothole_id: 3
+    }
+
+    const testPic3 = {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Newport_Whitepit_Lane_pot_hole.JPG/1920px-Newport_Whitepit_Lane_pot_hole.JPG',
+      pothole_id: 3
+    }
+
+    chai.request(app)
+      .get('/api/v1/pictures')
+      .end((req, res) => {
+        console.log(res.body);
+        expect(res.status).to.be.eql(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body.length).to.be.eql(5);
+        expect(res.body[0].url).to.be.eql('https://www.attorneystevelee.com/wp-content/uploads/pothole-road1.jpg');
+        expect(res.body[0].pothole_id).to.be.eql(1);
+      })
+  })
 })
