6de62bc56690723ba74fcffd76ed25a186c40580
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-04-18 14:17:03 -0600

    Create different test for a single pothole

--- test/server_spec.js
+++ test/server_spec.js
@@ -56,30 +56,55 @@ describe('Pot Spot API routes!', () => {
 
 
   it('should return all the photos @ /api/v1/pictures', () => {
-    const testPic1 = {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Asphalt_deterioration.jpg/1024px-Asphalt_deterioration.jpg',
-      pothole_id: 2
-    }
-
-    const testPic2 = {
-      url: 'https://www.thebalance.com/thmb/VlnrT3pRKvtegoumE0fXWmA4pWI=/2121x1193/smart/filters:no_upscale()/pothole-174662203-5a7dc84aae9ab80036c6ad36.jpg',
-      pothole_id: 3
-    }
-
-    const testPic3 = {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Newport_Whitepit_Lane_pot_hole.JPG/1920px-Newport_Whitepit_Lane_pot_hole.JPG',
-      pothole_id: 3
-    }
-
     chai.request(app)
       .get('/api/v1/pictures')
       .end((req, res) => {
-        console.log(res.body);
         expect(res.status).to.be.eql(200);
         expect(res.body).to.be.an('array');
         expect(res.body.length).to.be.eql(5);
         expect(res.body[0].url).to.be.eql('https://www.attorneystevelee.com/wp-content/uploads/pothole-road1.jpg');
         expect(res.body[0].pothole_id).to.be.eql(1);
       })
+  })
+
+
+  it('should return pothole and pictures @ /ai/v1/potholes/:id', () => {
+    chai.request(app)
+    .get('/api/v1/potholes/1')
+    .end((req, res) => {
+      expect(res.status).to.be.eql(200);
+      expect(res.body).to.be.a('object');
+      expect(res.body).to.have.property('id');
+      expect(res.body.id).to.be.eql(1);
+      expect(res.body).to.have.property('latitude');
+      expect(res.body.latitude).to.be.eql('39.74379494415912');
+      expect(res.body).to.have.property('longitude');
+      expect(res.body.longitude).to.be.eql('-104.95005172109876');
+      expect(res.body).to.have.property('description');
+      expect(res.body.description).to.be.eql('Decent size')
+      expect(res.body).to.have.property('pictures');
+      expect(res.body.pictures).to.be.an('array');
+      expect(res.body.pictures.length).to.be.eql(2);
+    })
+  })
+
+  it('should return a different pothole and pictures @ /ai/v1/potholes/:id', () => {
+    chai.request(app)
+    .get('/api/v1/potholes/2')
+    .end((req, res) => {
+      expect(res.status).to.be.eql(200);
+      expect(res.body).to.be.a('object');
+      expect(res.body).to.have.property('id');
+      expect(res.body.id).to.be.eql(2);
+      expect(res.body).to.have.property('latitude');
+      expect(res.body.latitude).to.be.eql('39.74018534594094');
+      expect(res.body).to.have.property('longitude');
+      expect(res.body.longitude).to.be.eql('-104.95724927698312');
+      expect(res.body).to.have.property('description');
+      expect(res.body.description).to.be.eql('its a pothole')
+      expect(res.body).to.have.property('pictures');
+      expect(res.body.pictures).to.be.an('array');
+      expect(res.body.pictures.length).to.be.eql(1);
+    })
   })
 })
